5643be112921c81d40b0e56127e641fb6ec51797
mikotopalmer139-del <mikotopalmer139@gmail.com>
2026-02-01 23:42:42 +1300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ function move() {
     const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
     noBtn.style.left = `${x}px`;
     noBtn.style.top = `${y}px`;
+    noBtn.style.position = 'fixed';
 }
 
 noBtn.addEventListener('touchstart', (e) => { e.preventDefault(); move(); });
 noBtn.addEventListener('mouseover', move);
 
 yesBtn.addEventListener('click', () => {
-    document.body.innerHTML = '<h1 style="text-align:center; margin-top:20%;">YAY! 💖</h1>';
+    document.body.innerHTML = '<h1 style="text-align:center; margin-top:20%;">YAY! 💖</h1><center><img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbmZ0eXoxY284bm8waG14M3kxbXo5eHh4eHh4eHh4eHh4eHh4eHh4JmlmPTExJmN0PWc/5GoVLqeAOo6PK/giphy.gif"></center>';
 });
